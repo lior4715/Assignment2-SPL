0f48dba5b5bd994cc7c141e2e86a4ca31e05177c
lior4715 <lior4715@gmail.com>
2025-12-28 21:58:34 +0200

    implemented run and loadAndCompute + fixed createtasks

--- src/main/java/spl/lae/LinearAlgebraEngine.java
+++ src/main/java/spl/lae/LinearAlgebraEngine.java
@@ -19,18 +19,74 @@ public class LinearAlgebraEngine {
 
     public ComputationNode run(ComputationNode computationRoot) {
         // TODO: resolve computation tree step by step until final matrix is produced
-        return null;
+        while(computationRoot.getNodeType() != ComputationNodeType.MATRIX){
+            computationRoot.associativeNesting();
+            ComputationNode resolvableNode = computationRoot.findResolvable();
+            loadAndCompute(resolvableNode);
+            double[][] resultData = resolvableNode.getChildren().get(0).getMatrix();
+            if(resolvableNode == computationRoot){
+                return computationRoot;
+            } else {
+                resolvableNode.resolve(resultData);
+            }
+        }
+        return computationRoot;
     }
 
     public void loadAndCompute(ComputationNode node) {
         // TODO: load operand matrices
         // TODO: create compute tasks & submit tasks to executor
+        if (node.getChildren().size() < 1) {
+            throw new IllegalArgumentException("Left child is null for node: " + node.getNodeType());
+        }
+        
+        double[][] leftData = node.getChildren().get(0).getMatrix();
+        leftMatrix.loadRowMajor(leftData);
 
+        if (node.getChildren().size() < 2 && 
+            (node.getNodeType() == ComputationNodeType.ADD || 
+             node.getNodeType() == ComputationNodeType.MULTIPLY)) {
+            throw new IllegalArgumentException("Right child is null for binary operation node: " + node.getNodeType());  
+        }
+
+        if(node.getChildren().size() > 1 && (node.getNodeType() == ComputationNodeType.NEGATE ||
+           node.getNodeType() == ComputationNodeType.TRANSPOSE)) {
+            throw new IllegalArgumentException("Unary operation node has two children: " + node.getNodeType());
+        }
+
+        double[][] rightData = node.getChildren().get(1).getMatrix();
+        rightMatrix.loadRowMajor(rightData);
+
+        if(node.getChildren().size() > 2){
+            throw new IllegalArgumentException("Node has more than two children: " + node.getNodeType());
+        }
+
+        List<Runnable> tasks = null;
+        switch (node.getNodeType()) {
+            case ADD:
+                tasks = createAddTasks();
+                break;
+            case MULTIPLY:
+                tasks = createMultiplyTasks();
+                break;
+            case NEGATE:
+                tasks = createNegateTasks();
+                break;
+            case TRANSPOSE:
+                tasks = createTransposeTasks();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported operation: " + node.getNodeType());
+        }
+        executor.submitAll(tasks);
     }
 
     public List<Runnable> createAddTasks() {
         // TODO: return tasks that perform row-wise addition
         List<Runnable> tasks = new java.util.ArrayList<>();
+        if(leftMatrix.length() != rightMatrix.length() || leftMatrix.get(0).length() != rightMatrix.get(0).length()) {
+            throw new IllegalArgumentException("Matrices must have the same number of rows to add.");
+        }
         for (int i = 0; i < leftMatrix.length(); i++) {
             final int rowIndex = i;
             tasks.add(() -> {
@@ -43,6 +99,9 @@ public class LinearAlgebraEngine {
     public List<Runnable> createMultiplyTasks() {
         // TODO: return tasks that perform row × matrix multiplication
         List<Runnable> tasks = new java.util.ArrayList<>();
+        if(leftMatrix.get(0).length() != rightMatrix.length()) {
+            throw new IllegalArgumentException("Number of columns in left matrix must equal number of rows in right matrix for multiplication.");
+        }
         for (int i = 0; i < leftMatrix.length(); i++) {
             final int rowIndex = i;
             tasks.add(() -> {
